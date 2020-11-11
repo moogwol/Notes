345d316300054e6f4652191ce429daf5fa994074
Miguel Corkhill <moogwol@msn.com>
2020-11-11 22:34:47 +0100

    Added the ability for the user to input a new note in the terminal

--- src/notes/Main.java
+++ src/notes/Main.java
@@ -3,8 +3,7 @@ package notes;
 public class Main {
     public static void main(String[] args) {
         NoteManager manager = new NoteManager();
-        Note note = manager.createNote("This is a new note");
-        System.out.println(note.getText());
-        System.out.println(note.getDateCreated());
+        Note newNote = manager.createNote();
+        manager.storeNote(newNote);
     }
 }
